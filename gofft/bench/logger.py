7946f5b01759e2c6ec435035ba4d7470fa48f938
NaleRaphael <gmccntwxy@gmail.com>
2018-07-22 12:45:16 +0800

    DOC: complete docstring of `LogWriter` and `LogReader`

--- gofft/bench/logger.py
+++ gofft/bench/logger.py
@@ -10,6 +10,16 @@ class LogWriter(object):
     User can create a custom writer by inheriting this class.
     """
     def write(self, fn, content, delimiter=','):
+        """
+        Parameters
+        ----------
+        fn : str
+            Path of log file.
+        content : ndarray
+            Content to be written into log file.
+        delimiter : str
+            The string used to separate values in `content`.
+        """
         np.savetxt(fn, content, delimiter=delimiter)
 
 
@@ -24,6 +34,15 @@ class LogReader(object):
         ----------
         fn : str
             Path of log file.
+        delimiter : str
+            The string used to separate values.
+
+        Returns
+        -------
+        x : ndarray
+            Data length of each step in benchmark.
+        y : ndarray
+            Elapsed time of each step in benchmark.
         """
         content = np.loadtxt(fn, delimiter=delimiter)
         x = content[:, 0]
